e7baa3ff41cdb82d16cf95b8ac56a2579bc40d1f
elephant-is-elepahnt <78339081+elephant-is-elepahnt@users.noreply.github.com>
2021-02-17 23:04:29 +0900

    Update Linear Probing hash table.py

--- chapter7/Linear Probing hash table.py
+++ chapter7/Linear Probing hash table.py
@@ -8,28 +8,42 @@ class hashtable():
         i = 0
         while self.table[hash_] is not None:
             i += 1
-            hash_ = self.find_hash(x+i)
+            hash_ = (x+i) % self.capacity
             
         return hash_
         
     def insert(self, x):
-        hash_ = self.find_hash(x)
-        
+        hash_ = x % self.capacity
+        i = 0
+        while self.table[hash_] is not None and self.table[hash_] != 'DELETED':
+            i += 1
+            hash_ = (x+i) % self.capacity
         self.table[hash_] = x
         
         return
     
-    def search(self, x):
-        hash_ = self.find_hash(x)
+    def search_loc(self, x):
+        hash_ = x % self.capacity
+        i = 0
+        if self.table[hash_] == x:
+            return hash_
         
-        return True
+        while self.table[hash_] is not None:
+            i += 1
+            hash_ = (x+i) % self.capacity
+            if self.table[hash_] == x:
+                return hash_
+            
+        return False
         
         
     def delete(self, x):
-        hash_ = self.find_hash(x)
-        self.table[hash_] = None
-        
-        return True
+        hash_ = self.search_loc(x)
+        if hash_ != False:
+            self.table[hash_] = 'DELETED'
+        else:
+            
+            return False
     
     def dump(self):
         for i in range(self.capacity):
