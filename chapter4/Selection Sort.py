6cba4c84a2c04927da1216d569cb1896e4082749
elephant-is-elepahnt <78339081+elephant-is-elepahnt@users.noreply.github.com>
2021-02-17 21:44:13 +0900

    Create Selection Sort.py

---
+++ chapter4/Selection Sort.py
@@ -0,0 +1,13 @@
+def SelectionSort(x):
+    last = len(x)-1
+    for n in range(last, 1, -1):
+        max_ = x[n]
+        loc = n
+        for j in range(n):
+            if max_<x[j]:
+                max_ = x[j]
+                loc = j
+        temp = x[n]
+        x[n] = max_
+        x[loc] = temp
+    return x
